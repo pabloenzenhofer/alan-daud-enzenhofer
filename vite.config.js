3da7c493b0ead9e0a965f8132bd1818550ae763d
pabloenzenhofer <pabloenzenhofer@gmail.com>
2024-12-30 20:24:48 -0300

    Build en carpeta docs para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/alan-daud-enzenhofer/',
+  build: {
+    outDir: 'docs'
+  },
   css: {
     modules: false
   },
